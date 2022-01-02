9f8b8f9100ea0f3315dfed406e8536d9a3969725
liquid8796 <hanam.tranle.5@gmail.com>
2022-01-02 11:20:28 +0700

    11:20 02/01/22

---
+++ Part 2 - Regression/Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  1 20:38:22 2022
+
+@author: Liquid
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
